6317fd04fb4c7fcded5a74cbc1f13d28ac9bcb64
Jesse Kerkvliet <s1082434@student.hsleiden.nl>
2015-12-09 15:03:39 +0100

    PDF generator (eerste versie) toegevoegd
    PDF generator met LateX voor pdf

---
+++ Scripts/generating.py
@@ -0,0 +1,37 @@
+import argparse
+import subprocess
+import os
+import datetime
+firstpage=r'''\documentclass{article}
+\begin{document}
+\textbf{\Huge %(title)s \\}
+\vspace{1cm}
+\textbf{\large %(name)s \\}
+\textbf{\normalsize %(student_num)s \\}
+\textbf{\normalsize %(month)s \\}
+\end{document}
+'''
+now = datetime.datetime.now()
+parser = argparse.ArgumentParser()
+parser.add_argument('-num','--student_num')
+parser.add_argument('-n','--name')
+parser.add_argument('-t','--title')
+parser.add_argument('-s','--school', default="Hogeschool Leiden")
+parser.add_argument("ss")
+
+args=parser.parse_args()
+argdict = args.__dict__
+month_year = "Pizza"
+month_year = "%s %s"%(str(now.strftime("%B")),str(now.year))
+print month_year
+argdict["month"] = month_year
+print args
+print args.__dict__
+firstpage%argdict
+
+with open('firstpage.tex','w') as f:
+    f.write(firstpage%args.__dict__)
+
+os.system('pdflatex firstpage.tex')
+
+os.unlink('firstpage.log')
