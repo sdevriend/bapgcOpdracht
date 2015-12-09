700a9cb1c773281873b2178effa14d8afca34c9d
Jesse Kerkvliet <s1082434@student.hsleiden.nl>
2015-12-09 16:40:45 +0100

    Functions toegevoegd
    Als als ss(subsection) FP wordt gekozen, wordt de voorkant gegenereerd.

--- Scripts/generating.py
+++ Scripts/generating.py
@@ -2,41 +2,54 @@ import argparse
 import subprocess
 import os
 import datetime
-firstpage=r'''
+def front_page(argdict):
+    now = datetime.datetime.now()
+    firstpage=r'''
+    \documentclass{article}
+    \usepackage{graphicx}
+    \begin{document}
+    \textbf{\Huge %(title)s \\}
+    \vspace{1cm}
+    \begin{figure}[h]
+    \centering
+    \includegraphics[height=10cm, width=10cm]{test}
+    \end{figure}
+    \vspace{3cm}
+    \textbf{\large %(name)s \\}
+    \vspace{1cm}
+    \textbf{\normalsize %(student_num)s \\}
+    \textbf{\normalsize %(month)s \\}
+    \end{document}
+    '''
+    month_year = "%s %s"%(str(now.strftime("%B")),str(now.year))
+    argdict["month"] = month_year
+    texfile = firstpage%argdict
+    return texfile
+def main_function():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-num','--student_num')
+    parser.add_argument('-n','--name')
+    parser.add_argument('-t','--title')
+    parser.add_argument("ss")
 
-\documentclass{article}
-\usepackage{graphicx}
-\begin{document}
-\textbf{\Huge %(title)s \\}
-\vspace{1cm}
-\includegraphics[height=10cm, width=10cm]{Pathwayimage}
-\vspace{2cm}
-\textbf{\large %(name)s \\}
-\vspace{1cm}
-\textbf{\normalsize %(student_num)s \\}
-\textbf{\normalsize %(month)s \\}
-\end{document}
-'''
-now = datetime.datetime.now()
-parser = argparse.ArgumentParser()
-parser.add_argument('-num','--student_num')
-parser.add_argument('-n','--name')
-parser.add_argument('-t','--title')
-parser.add_argument("ss")
+    args=parser.parse_args()
+    argdict = args.__dict__
+    
+    if (argdict["ss"] == "FP"):
+        texfile = front_page(argdict)
+    with open('firstpage.tex','w') as f:
+        pass
+    writefile(texfile)
+    execute()
+def writefile(texfile):
+    with open('firstpage.tex','a') as f:
+        f.write(texfile)
 
-args=parser.parse_args()
-argdict = args.__dict__
-month_year = "Pizza"
-month_year = "%s %s"%(str(now.strftime("%B")),str(now.year))
-print month_year
-argdict["month"] = month_year
-print args
-print args.__dict__
-firstpage%argdict
+def execute():
+    os.system('pdflatex firstpage.tex')
 
-with open('firstpage.tex','w') as f:
-    f.write(firstpage%args.__dict__)
 
-os.system('pdflatex firstpage.tex')
+if __name__ == "__main__":
+    main_function()
 
-os.unlink('firstpage.log')
