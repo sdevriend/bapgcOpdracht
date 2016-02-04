dd8c110da04a6c9aa733bf0b76298b798255829c
sdevriend <sebastiaandevriend@gmail.com>
2016-02-04 13:00:11 +0100

    Added Hydroscript to pipeline
    Still needs to be tested...

--- Scripts/Hydrofobiciteit.R
+++ Scripts/Hydrofobiciteit.R
@@ -1,3 +1,4 @@
+#!/usr/bin/Rscript
 library("GenomicRanges")
 library("BSgenome.Hsapiens.UCSC.hg38")
 library(Biostrings)
@@ -5,27 +6,29 @@ library(org.Hs.eg.db)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
 
-setwd("C:/Users/jesse/Documents/Bio-informatica/Jaar 3/Periode 2/Bapgc/")
-#Gene database en genoom worden gedefinieerd
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-genome <- BSgenome.Hsapiens.UCSC.hg38
+PreferenceRatio <- function(){
+  #Gene database en genoom worden gedefinieerd
+  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+  genome <- BSgenome.Hsapiens.UCSC.hg38
 
-#Alle gevonden genen worden geladen en de eerste 10 worden meegenomen
-#De ID's worden omgezet naar Entrez Gene ID's 
-geneList <- load("AllCoregulatedGenes.csv")
-Protein.data <- fullFrame[c(1:10),]
-Protein.data <- getEntrez(Protein.data)
+  #Alle gevonden genen worden geladen en de eerste 10 worden meegenomen
+ #De ID's worden omgezet naar Entrez Gene ID's 
+  geneList <- load("AllCoregulatedGenes.csv")
+  Protein.data <- fullFrame[c(1:10),]
+  Protein.data <- getEntrez(Protein.data)
 
-# Alle exons van de genen worden geladen,
-# hiervan wordt de sequentie geladen
-# De sequenties worden getransleerd naar AA sequenties
-exons <- exonsBy(txdb, by="gene")[Protein.data]
-protein.dna <- getSeq(genome, exons)
-Protein.AA <- translate(unlist(protein.dna))
+  # Alle exons van de genen worden geladen,
+  # hiervan wordt de sequentie geladen
+  # De sequenties worden getransleerd naar AA sequenties
+  exons <- exonsBy(txdb, by="gene")[Protein.data]
+  protein.dna <- getSeq(genome, exons)
+  Protein.AA <- translate(unlist(protein.dna))
 
-#De hydrofobiciteit wordt geteld en de barplot wordt gegenereerd
-dfAmounts <- getRatios(Protein.AA)
-generateBarplot(Protein.AA, dfAmounts)
+  #De hydrofobiciteit wordt geteld en de barplot wordt gegenereerd
+  dfAmounts <- getRatios(Protein.AA)
+  generateBarplot(Protein.AA, dfAmounts)
+
+}
 
 getEntrez <- function(dfGenes){
   #De functie splitst de ID's op een punt en de unieke lijst
@@ -65,14 +68,34 @@ getRatios <- function(AAStringSet){
 }
 
 generateBarplot <- function(Protein.AA, dfAmounts){
-dfNew <- matrix(ncol=3)
+  dfNew <- matrix(ncol=3)
 
-dfAmounts.t <- t(dfAmounts)
-rownames(dfAmounts.t) <- c("Hydrophoob","Hydrofiel","Totaal")
-colnames(dfAmounts.t) <- unique(names(Protein.AA))
-barplot(dfAmounts.t, main="Verdeling hydrofobiciteit in gecoreguleerde genen",
+  dfAmounts.t <- t(dfAmounts)
+  png("Hydrofobiciteit.png")
+  rownames(dfAmounts.t) <- c("Hydrophoob","Hydrofiel","Totaal")
+  colnames(dfAmounts.t) <- unique(names(Protein.AA))
+  barplot(dfAmounts.t, main="Verdeling hydrofobiciteit in gecoreguleerde genen",
         xlab="Entrez ID", ylab="Frequentie", col=c("darkblue","blue","lightblue"),
         legend = rownames(dfAmounts.t), beside=FALSE)
-
+  dev.off()
 }
 
+main <- function(args)
+{
+  if (length(args) > 0)
+  {
+    if (args[1] == "-h" |  args[1] == "-help" | args[1] == "--h" | args[1] == "--help")
+    {
+      showUsageInformation()
+    }
+    else
+    {
+      setwd(args[1])
+      PreferenceRatio()
+    }
+  }
+  
+}
+
+
+main(commandArgs(T))
