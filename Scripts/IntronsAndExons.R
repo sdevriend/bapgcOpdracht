06b09f902273d47662559d8aa494904870b540e8
sdevriend <sebastiaandevriend@gmail.com>
2016-02-04 13:32:58 +0100

    Alteration for IntronsAndExons
    Ready making for pipeline

--- Scripts/IntronsAndExons.R
+++ Scripts/IntronsAndExons.R
@@ -4,44 +4,37 @@ library(org.Hs.eg.db)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library("hgu95av2.db")
 library("BSgenome.Hsapiens.UCSC.hg38")
+library(png)
+IntroExons <- function(){
+  #Gene database en genoom worden gedefinieerd
+  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+  genome <- BSgenome.Hsapiens.UCSC.hg38
+  load("AllCoregulatedGenes.csv")
 
-setwd("C:/Users/jesse/Documents/Bio-informatica/Jaar 3/Periode 2/Bapgc/")
-#Gene database en genoom worden gedefinieerd
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-genome <- BSgenome.Hsapiens.UCSC.hg38
-load("AllCoregulatedGenes.csv")
+  geneset <- fullFrame[c(1:20),]
+  geneset <- getEntrez(geneset)
+  lengths.ma <- matrix(ncol=2)
+  i<-1
 
-geneset <- fullFrame[c(1:20),]
-geneset <- getEntrez(geneset)
-lengths.ma <- matrix(ncol=2)
-i<-1
-
-for(i in seq(1, length(geneset))){
+  for(i in seq(1, length(geneset))){
   
-  #lengths <- tryCatch({
-  exon <- exonsBy(txdb, by="gene")[geneset[i]]
-  exon.seq <- unlist(getSeq(genome, exon))
-  exon.length <- sum(width(exon.seq))
-  transcripts <- transcriptsBy(txdb, by="gene")[geneset[i]]
-  transcripts.seq <- unlist(getSeq(genome, transcripts))
-  intron.length <- sum(width(transcripts.seq))-exon.length
-  print(intron.length)
-  #print(paste(exon.length, intron.length))
-  #print("YOOOO")
-  length <- c(exon.length, intron.length)
-  print(length)
-  lengths.ma <- rbind(lengths.ma, length)
-  #return(length)
-  #},
- # error = function(err){
-  #print("NAh")
- # return(c(NA,NA))
-#})
-
-}
-dflengths <- as.data.frame(lengths.ma[-1,])
-plot(dflengths$V2, dflengths$V1, xlab="Intronlengte", ylab="Exonlengte",
+    #lengths <- tryCatch({
+    exon <- exonsBy(txdb, by="gene")[geneset[i]]
+    exon.seq <- unlist(getSeq(genome, exon))
+    exon.length <- sum(width(exon.seq))
+    transcripts <- transcriptsBy(txdb, by="gene")[geneset[i]]
+    transcripts.seq <- unlist(getSeq(genome, transcripts))
+    intron.length <- sum(width(transcripts.seq))-exon.length
+    length <- c(exon.length, intron.length)
+    lengths.ma <- rbind(lengths.ma, length)
+  }
+  dflengths <- as.data.frame(lengths.ma[-1,])
+  png("IntronsExons.png")
+  plot(dflengths$V2, dflengths$V1, xlab="Intronlengte", ylab="Exonlengte",
      main="Scatterplot van 20 gecorelugeerde genen")
+  dev.off()
+	 
+}
 getEntrez <- function(dfGenes){
   #De functie splitst de ID's op een punt en de unieke lijst
   #Wordt teruggegeven
@@ -49,3 +42,23 @@ getEntrez <- function(dfGenes){
   entrez <- unique(entrez)
   return(entrez)
 }
+
+main <- function(args)
+{
+  if (length(args) > 0)
+  {
+    if (args[1] == "-h" |  args[1] == "-help" | args[1] == "--h" | args[1] == "--help")
+    {
+      showUsageInformation()
+    }
+    else
+    {
+      setwd(args[1])
+      IntroExons()
+    }
+  }
+  
+}
+
+
+main(commandArgs(T))
