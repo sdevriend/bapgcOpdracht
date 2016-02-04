f9fc6dae99e74c3af809e1b807c7ac58dbc38084
sdevriend <sebastiaandevriend@gmail.com>
2016-02-04 12:34:11 +0100

    Added png support to EnzymesWIthFunctions
    needs to be tested

--- Scripts/EnzymesWithFunctions.R
+++ Scripts/EnzymesWithFunctions.R
@@ -1,6 +1,7 @@
 #!/usr/bin/Rscript
 library("hgu95av2.db")
 library(limma)
+library(png)
 Enzymes <- function(){
   load("AllCoregulatedGenes.csv")
 
@@ -67,10 +68,12 @@ generateVennDiagram <- function(enzymeset){
   #De twee logische klassen worden vergeleken en een venndiagram wordt gemaakt
   c3 <- cbind(totalEnzyms, noFuncs)
   count <- vennCounts(c3)
+  png(file="VennDiagram.png")
   vennDiagram(count, mar=c(1,1,1,1),
               names=c("Enzymen","Enzymes zonder bekende functie"), 
               main="Enzymen zonder functie in de set",
               lwd=2, circle.col="blue", cex=1)
+  dev.off()
   }
   
 main <- function(args)
