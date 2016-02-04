f9c0962033188bc49d8b0d0873b218157213dd1a
Jesse Kerkvliet <s1082434@student.hsleiden.nl>
2016-02-04 22:54:19 +0100

    Documentatie
    En twee functies weggelazerd. Efficiënt scripten is niet mijn ding.

--- Scripts/EnzymesWithFunctions.R
+++ Scripts/EnzymesWithFunctions.R
@@ -2,81 +2,65 @@
 library("hgu95av2.db")
 library(limma)
 library(png)
+showUsageInformation <- function()
+{
+  print("") 
+  print("Het script genereert een venndiagram")
+  print("Aan de hand van de honderd beste medegereguleerde genen.")
+  print("Het diagram toont de hoeveelheid enzymen in de set")
+  print("en de hoeveelheid enzymen zonder bekende functie")
+  print("Het script kan aangeroepen worden door EnzymesWithFunctions.R")
+  print("")
+  quit()
+}
 Enzymes <- function(){
-  load("AllCoregulatedGenes.csv")
-
-  # Neemt de honderd beste hits van een genenset (in dit geval een testvar)
-  HundredBestHits <- fullFrame[c(1:100),]
-  geneset <- getEntrez(HundredBestHits)
-  # Genereert annotaties (Entrez, Symbol, Ensembl, functie en EC-nummer)
-  annotations <- getEnzymes(unlist(HundredBestHits))
-  # Haalt alle annotaties met een EC-nummer op
-  enzymes <- annotations[!is.na(annotations$ENZYME),]
-  # Zoekt naar hypotheticals, unknowns en uncharacterizeds.
-  funcless <- getEnzymesWithKnownFunctions(annotations)
-  # Genereert venn diagram
-  generateVennDiagram(enzymes)
+	# Alle gevonden medegereguleerde genen worden ingeladen.
+	# De honderd bovenste hits van de lijst worden genomen en de 
+	# ID's worden omgezet naar Entrez-gene ID's
+	# De annotaties worden opgehaald. Hierin zitten de Entrez-gene ID,
+	# functie en EC-nummer. Alle annotaties met een EC-nummer worden 
+	# opgehaald. De hypothetical, unknowns en uncharaterizeds worden
+	# gezocht en verwijdert en de functie om een venndiagram te genereren
+	# wordt aangeroepen.
+	load("AllCoregulatedGenes.csv")
+	vctHundredBestHits <- fullFrame[c(1:100),]
+	vctGeneset <- getEntrez(vctHundredBestHits)
+	dfAnnotations<- select(hgu95av2.db, key=entrez, keytype="ENTREZID", 
+                      columns=c("GENENAME","ENZYME", 
+                                "ENTREZID"))
+	dfEnzymes <- dfAnnotations[!is.na(dfAnnotations$ENZYME),]
+	funcless <- getEnzymesWithKnownFunctions(dfAnnotations)
+	generateVennDiagram(dfEnzymes)
 }
 
 getEntrez <- function(dfGenes){
-  #De functie splitst de ID's op een punt en de unieke lijst
-  #Wordt teruggegeven
-  entrez <- unlist(strsplit(as.character(dfGenes), '\\.'))
-  entrez <- unique(entrez)
-  return(entrez)
-}
-
-getEnzymes <- function(geneset){
-  #De entrezID's worden genomen door de set te splitsen op een punt
-  entrez <- unlist(strsplit(as.character(geneset), "\\."))
-  entrez <- unique(entrez)
-  #De duplicaten worden verwijderd
-  #De annotaties worden geselecterd
-  annotation<- select(hgu95av2.db, key=entrez, keytype="ENTREZID", 
-                      columns=c("SYMBOL", "ENSEMBL","GENENAME","ENZYME", 
-                                "ENTREZID"))
-  #Geeft de annotatie terug
-  return (annotation)
-}
-
-getEnzymesWithKnownFunctions<-function(enzymeset){
-    #Haalt alle enzymen zonder functie op
-    noFuncs <- grepl("uncharacterized | hypothetical | ^unknown$", enzymeset$GENENAME)
-    #Er wordt gekeken naar de functieloze enzymen. Deze worden opgeslagen in een
-    #lijst. De lijst wordt ontdaan van NA's en teruggegeven.    
-    noFuncs2 <- c()
-    for(i in seq(1,(length(noFuncs)))){
-    
-    if(noFuncs[i] == TRUE){
-      print(enzymeset$SYMBOL[i])
-      noFuncs2[[i]] <- enzymeset$SYMBOL[i]
-    }
-  }
-  noFuncs2 <- noFuncs2[!is.na(noFuncs2)]
-  return(unique(noFuncs2))
-  
+    #De functie splitst de ID's op een punt en de unieke lijst
+    #Wordt teruggegeven
+    vctEntrez <- unlist(strsplit(as.character(dfGenes), '\\.'))
+    vctEntrez <- unique(entrez)
+    return(vctEntrez)
 }
 
 
-generateVennDiagram <- function(enzymeset){
-  #De functieloze enzymen worden gezocht
-  noFuncs <- grepl("uncharacterized | hypothetical | ^unknown$", enzymeset$GENENAME)
-  
-  #Een logical object wordt gemaakt met alle enzymen die een EC-nummer hebben
-  #(dat zijn ze allemaal)
-    totalEnzyms <- (!is.na(enzymeset$ENZYME))
-  #De twee logische klassen worden vergeleken en een venndiagram wordt gemaakt
-  c3 <- cbind(totalEnzyms, noFuncs)
-  count <- vennCounts(c3)
-  png("VennDiagram.png")
-  vennDiagram(count, mar=c(1,1,1,1),
-              names=c("Enzymen","Enzymes zonder bekende functie"), 
-              main="Enzymen zonder functie in de set",
-              lwd=2, circle.col="blue", cex=1)
-			  
-  #png(file="VennDiagram.png")
-  #writePNG(png, )
-  dev.off()
+
+generateVennDiagram <- function(dfEnzymeset){
+    #De functieloze enzymen worden gezocht,
+	#Een logical object wordt gemaakt met alle enzymen die een EC-nummer hebben
+	#(dat zijn ze allemaal, het typeert een enzym)
+	#De twee logische klassen worden vergeleken en een venndiagram wordt gemaakt
+	dfNoFuncs <- grepl("uncharacterized | hypothetical | ^unknown$",
+        dfEnzymeset$GENENAME)
+	dfTotalEnzyms <- (!is.na(dfEnzymeset$ENZYME))
+
+	mtDelen <- cbind(totalEnzyms, noFuncs)
+	count <- vennCounts(mtDelen)
+	png("VennDiagram.png")
+	vennDiagram(count, mar=c(1,1,1,1),
+        names=c("Enzymen","onbekende functie"), 
+        main="Enzymen zonder functie in de set",
+        lwd=2, circle.col="blue", cex=1)
+
+	dev.off()
   }
   
 main <- function(args)
@@ -98,3 +82,12 @@ main <- function(args)
 
 
 main(commandArgs(T))
+
+# Additional information:
+# =======================
+# Dit script is geschreven door Jesse Kerkvliet en
+# kan gebruikt worden bij de opdracht van bapgc door
+# Sebastiaan de Vriend.
+#
+# Het script genereert een venndiagram met enzymen zonder bekende functie.
+# en slaat deze op als afbeelding
