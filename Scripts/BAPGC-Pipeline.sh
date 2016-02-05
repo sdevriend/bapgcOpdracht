c0a5d73c52f9e650ad7da61761a2ef9aa9865897
sdevriend <sebastiaandevriend@gmail.com>
2016-02-05 16:16:56 +0100

    BAPGC-ding. bijna af

--- Scripts/BAPGC-Pipeline.sh
+++ Scripts/BAPGC-Pipeline.sh
@@ -41,28 +41,44 @@ then
     echo "Klaar met downloaden"
 fi
 echo "Ophalen Pathway info van Kegg."
-./GenesFromPathway.r ${TempDir} ${Pathway}
+#./GenesFromPathway.r ${TempDir} ${Pathway}
 echo "Ophalen pathway informatie is klaar."
 echo "Starten promoter script."
-./PromoterSeqsFromGenes.R ${TempDir}
+#./PromoterSeqsFromGenes.R ${TempDir}
 echo "Promotor sequenties zijn gevonden. Door met motifs zoeken."
-./MotifsFromPromoterSequences.R ${TempDir}
+#./MotifsFromPromoterSequences.R ${TempDir}
 echo "Genen zoeken..."
-./GenesFromMotifs.R ${TempDir}
+#./GenesFromMotifs.R ${TempDir}
 echo "Genen gevonden. Zoeken naar genen met functie."
-./EnzymesWithFunctions.R ${TempDir}
+#./EnzymesWithFunctions.R ${TempDir}
 echo "Enzymen gevonden. Bepalen Hydrofobic eigenschappen..."
-./Hydrofobiciteit.R ${TempDir}
+#./Hydrofobiciteit.R ${TempDir}
 echo "Hydrofobic eigenschappen opgezocht."
 echo "Door met intron/exon bepaling."
-./IntronsAndExons.R ${TempDir}
+#./IntronsAndExons.R ${TempDir}
 echo "Maken MSA."
-./MSA.R ${TempDir}
+#./MSA.R ${TempDir}
 echo "Testen phylo"
-./PhylogenyOfParalogs.R ${TempDir}
+#./PhylogenyOfParalogs.R ${TempDir}
 # Maken van multi fasta met de volgende twee pipes:
-cat ${TempDir}/*.fasta | awk '{if (substr($1,1,1) == ">") print $0" "; else print $1}' | tr -d "\n" | tr ">" "\n"  > ${TempDir}/multitemp.fa
-cat ${TempDir}/multitemp.fa  | awk '{if(NR>1)print $0}' | awk '{print ">"$0}' | sed s/" "/"\n"/  > ${TempDir}/tree.fa
-clustalw data -infile=${TempDir}/tree.fa -tree -outputtree=dist 
-./generateTree.R ${TempDir}
-echo "Klaar met script."
+#cat ${TempDir}/*.fasta | awk '{if (substr($1,1,1) == ">") print $0" "; else print $1}' | tr -d "\n" | tr ">" "\n"  > ${TempDir}/multitemp.fa
+#cat ${TempDir}/multitemp.fa  | awk '{if(NR>1)print $0}' | awk '{print ">"$0}' | sed s/" "/"\n"/  > ${TempDir}/tree.fa
+#clustalw data -infile=${TempDir}/tree.fa -tree -outputtree=dist 
+#./generateTree.R ${TempDir}
+echo "Klaar met script. Door met verslag."
+cp ${TempDir}/Hydrofobiciteit.png .
+cp ${TempDir}/IntronsExons.png .
+cp ${TempDir}/MSARESULT.txt .
+cp ${TempDir}/Pathway.png .
+cp ${TempDir}/tree.png .
+cp ${TempDir}/VennDiagram.png .
+cp ${TempDir}/FoundMotifs.csv .
+# Run latex script
+
+mv Hydrofobiciteit.png ${ResultDir}
+mv IntronsExons.png ${ResultDir}
+mv MSARESULT.txt ${ResultDir}
+mv Pathway.png ${ResultDir}
+mv tree.png ${ResultDir}
+mv VennDiagram.png ${ResultDir}
+mv FoundMotifs.csv ${ResultDir}
